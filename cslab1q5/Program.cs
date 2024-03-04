481ac439caaa2497a220fe330f712e4e70937f75
MINOLI <MINOLI@LAPTOP-VTQD26BJ>
2024-03-04 09:51:07 +0530

    Add project files.

---
+++ cslab1q5/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cslab1q5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter a number for multiplication table");
+            int n= int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"multiplication table of {n}");
+            for(int i = 1; i<= 10; i++) 
+            { 
+                Console.WriteLine($"{n} x {i} = {n *i}");
+            }
+            Console.ReadLine();
+        }
+    }
+}
